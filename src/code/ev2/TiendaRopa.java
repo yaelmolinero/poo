966bb50188877224f67d938293e3ce75ae4fd997
Yael Molinero <y.molinero20@gmail.com>
2025-05-07 15:51:13 -0600

    TiendaRopa mejorado

--- src/code/ev2/TiendaRopa.java
+++ src/code/ev2/TiendaRopa.java
@@ -13,11 +13,11 @@ class Ropa {
     this.discount = discount;
   }
 
-  public void addCantity(int cantity) {
-    this.cantity += cantity;
+  public void addCantity() {
+    this.cantity++;
 
-    totalPrice += cantity * price;
-    totalWithDiscount = totalPrice * discount;
+    this.totalPrice += this.cantity * this.price;
+    this.totalWithDiscount = this.totalPrice * this.discount;
   }
 }
 
@@ -32,60 +32,59 @@ public class TiendaRopa {
     Ropa pantalones = new Ropa("Pantalon", 600, discount);
     Ropa camisas = new Ropa("Camisa", 300, discount);
 
-    // Ropa[] compra = { vestidosConMangas, vestidosSinMangas, blusas, pantalones, camisas };
-
     System.out.println("TIENDA DE ROPA");
     int opcion;
     do {
-      System.out.println();
+      System.out.println("--------------------------");
       System.out.println("1. Vestidos");
       System.out.printf("2. Blusas ($%.2f) \n", blusas.price);
       System.out.printf("3. Pantalones ($%.2f) \n", pantalones.price);
       System.out.printf("4. Camisas ($%.2f) \n", camisas.price);
       System.out.println("5. Salir");
-      System.out.print("Eliga la prenda de ropa: ");
+      
+      System.out.print("\n-> Eliga la prenda de ropa: ");
       opcion = scanner.nextInt();
 
       switch (opcion) {
         case 1:
           System.out.printf("1. Con mangas ($%.2f) \n", vestidosConMangas.price);
           System.out.printf("2. Sin mangas ($%.2f) \n", vestidosSinMangas.price);
-          System.out.print("Opcion: ");
+
+          System.out.print("-> Opcion: ");
           int opcionVestido = scanner.nextInt();
 
-          if (opcionVestido == 1) vestidosConMangas.addCantity(1);
-          else if(opcionVestido == 2) vestidosSinMangas.addCantity(1);
+          if (opcionVestido == 1) vestidosConMangas.addCantity();
+          else if(opcionVestido == 2) vestidosSinMangas.addCantity();
           else System.out.println("Opcion invalida.");
           break;
 
-        case 2: blusas.addCantity(1); break;
-        case 3: pantalones.addCantity(1); break;
-        case 4: camisas.addCantity(1); break;
-        case 5: System.out.println("Saliendo..."); break;
-      
-        default:
-          System.out.println("Opcion invalida. Intente de nuevo.");
-          break;
+        case 2: blusas.addCantity();      break;
+        case 3: pantalones.addCantity();  break;
+        case 4: camisas.addCantity();     break;
+        case 5: System.out.println("Imprimiendo ticket..."); break;
+        default: System.out.println("Opcion invalida. Intente de nuevo."); break;
       }
       
     } while (opcion != 5);
 
     Ropa[] compras = { vestidosConMangas, vestidosSinMangas, blusas, pantalones, camisas };
-
     double total = 0;
+    System.out.println("\n-------------------------");
+
     for (Ropa compra: compras) {
       if (compra.cantity > 0) {
         System.out.printf(
-          "%s: %d x %.2f - 10 = %.2f\n", 
+          "%-18s %-2d x $%-2.2f - 10%% = $%.2f\n", 
           compra.name, compra.cantity, compra.price, compra.totalWithDiscount
         );
+
         total += compra.totalWithDiscount;
       }
     }
 
     System.out.println("-------------------------");
-    System.out.printf("Total: %.2f", total);
-
+    System.out.printf("Total a pagar: $%.2f\n", total);
+    System.out.println("-------------------------");
     scanner.close();
   }
 }
