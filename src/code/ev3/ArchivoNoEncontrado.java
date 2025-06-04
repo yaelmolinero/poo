5d5410e90cf32bcd7e30477fb2891ae2de3b4f7c
Yael Molinero <y.molinero20@gmail.com>
2025-06-04 10:56:27 -0600

    Ejercicio try/catch

--- src/code/ev3/ArchivoNoEncontrado.java
+++ src/code/ev3/ArchivoNoEncontrado.java
@@ -1,9 +1,55 @@
 package code.ev3;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
 
 public class ArchivoNoEncontrado {
   public static void main(String[] args) {
-    /*
-     * Crea un aplicacion con en java con swing, tendra dos inputs para ingresar un numero en cada uno, tambien debe tener un boton para calcular la division entre los dos numeros y mostrar el resultado en otro elemento de texto, tambien debe de tener otro boton para reiniciar los valores que se ingresaron en los dos inputs y eliminar el texto donde se mostro el resultado. La app debe tener un layout que se divide en dos columnas, la primera debe tener todos los elementos mencionados anteriormente y la otra debe contener una imagen o un gif
-     */
+    JFrame frame = new JFrame("Archivo No Encontrado");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(500, 300);
+    frame.setResizable(false);
+    frame.setLayout(new GridLayout(1, 2));
+
+    // Panel 1: Botón y mensaje
+    JPanel panel1 = new JPanel();
+    panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+    panel1.setBackground(Color.BLACK);
+    
+    JButton openButton = new JButton("Abrir archivo");
+    openButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+    JLabel messageLabel = new JLabel(" ");
+    messageLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+    messageLabel.setForeground(Color.WHITE);
+
+    openButton.addActionListener((e) -> {
+      try {
+        File file = new File("archivo.txt");
+          if (!file.exists()) throw new Exception("Archivo no encontrado");
+
+          messageLabel.setText("Archivo encontrado: " + file.getName());
+      } catch (Exception err) {
+          messageLabel.setText("Archivo no encontrado");
+      }
+    });
+
+    panel1.add(Box.createVerticalStrut(30));
+    panel1.add(openButton);
+    panel1.add(Box.createVerticalStrut(20));
+    panel1.add(messageLabel);
+
+    // Panel 2: Imagen
+    JPanel panel2 = new JPanel();
+    panel2.setLayout(new BorderLayout());
+
+    ImageIcon icon = new ImageIcon(ArchivoNoEncontrado.class.getResource("/img/bornfire.gif"));
+    JLabel imageLabel = new JLabel(icon, JLabel.CENTER);
+
+    panel2.add(imageLabel, BorderLayout.CENTER);
+
+    frame.add(panel1);
+    frame.add(panel2);
+    frame.setVisible(true);
   }
 }
--- src/code/ev3/README.md
+++ src/code/ev3/README.md
@@ -1,4 +1,5 @@
 ## Orden de los programas
 
 - 01: [Division](/src/code/ev3/Division.java)
+- 02: [ArchivoNoEncontrado](/src/code/ev3/ArchivoNoEncontrado.java)
 - 03: [Paneles](/src/code/ev3/Paneles.java)
